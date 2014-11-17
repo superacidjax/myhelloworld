34545093bf86b2fb58380913c18d1473f8ee71e1
Brian Dear <superacidjax@me.com>
2014-11-18 00:22:47 +0100

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  mount Commontator::Engine => '/commontator'
 
+  resources :charges
+  resources :courses do
+    resources :lessons
+  end
 
-
-  resources :courses
   resources :sessions
   resources :users
 
   get 'sign_in' => 'sessions#new', as: :sign_in
-  get 'sign_out' => 'sessions#destroy', as: :sign_out
+  delete 'sign_out' => 'sessions#destroy', as: :sign_out
   get 'sign_up' => 'users#new', as: :sign_up
+  get 'lesson_complete' => 'lessons#user_completed', as: :lesson_completed
 end

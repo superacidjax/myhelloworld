bc92e05c8c7dc1522af33fece7a2e33e8c457016
Brian Dear <superacidjax@me.com>
2014-11-28 21:50:17 +0100

    removes controller specs and useless fabricator

--- spec/fabricators/sale_fabricator.rb
+++
@@ -1,8 +0,0 @@
-Fabricator(:sale) do
-  user_id                1
-  guid                   "MyString"
-  stripe_id              "MyString"
-  course_id              1
-  access_expiration_date "2014-11-16 11:17:49"
-  price_paid             1
-end

402037552d62336fb441f87d5b4b6f5a9d79ae9c
Brian Dear <superacidjax@me.com>
2014-11-28 21:41:56 +0100

    adds pluralize without count helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,10 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, options).render(text).html_safe
   end
 
+  def pluralize_without_count(count, noun, text = nil)
+    if count != 0
+      count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
+    end
+  end
+
 end

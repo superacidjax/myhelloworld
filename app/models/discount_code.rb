40273d1dc0f84f601cef30683568ad7022f84955
Brian Dear <superacidjax@me.com>
2014-11-28 22:27:21 +0100

    validates discount code type

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -3,6 +3,11 @@ class DiscountCode < ActiveRecord::Base
 
   has_many :charges
 
+  DISCOUNT_TYPE = ['percent', 'subtraction']
+
+  validates_inclusion_of :discount_type, in: DISCOUNT_TYPE, allow_nil: false,
+    allow_blank: false
+
   def not_expired_or_cancelled?
     if self.cancelled?
       false

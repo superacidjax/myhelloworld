9e0510b0948e2cc0aa2037075b12f294ec129284
Brian Dear <superacidjax@me.com>
2014-11-29 17:40:34 +0100

    adds spec for viewing course listings

--- spec/features/user_views_course_listing_spec.rb
+++ spec/features/user_views_course_listing_spec.rb
@@ -10,22 +10,32 @@ feature 'User views courses' do
     second_course.save
   end
 
-  scenario 'User views course listing' do
+  scenario 'A visiting user views course listing' do
     visit root_path
     click_link 'See our courses'
     expect(page).to have_content course.name
     expect(page).to have_content second_course.name
+    expect(page).to have_content course.level
+    expect(page).to have_content course.programming_language
+    expect(page).to have_content course.lessons.count
+    expect(page).to have_link 'Read more'
   end
 
-  scenario 'User views course details' do
+  scenario 'A visiting user views course details' do
     visit courses_path
     click_link course.id
     expect(page).to have_content course.name
-    expect(page).to have_content course.price
+    expect(page).to have_content course.level
+    expect(page).to have_link 'Get Started Now'
+    expect(page).to have_content course.description
+    expect(page).to have_content course.headline
   end
 
-  scenario 'User successfully visits Vimeo' do
+  scenario 'A visiting user wants to get started' do
     visit course_path(course)
-    #test vimeo link here
+    click_link 'Get Started Now'
+    expect(page).to have_content 'It starts here.'
+    expect(page).to have_button 'Try for Free'
   end
+
 end

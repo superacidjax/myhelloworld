79149cdf8d7659fc7443add61b4a12256fae8dc0
Brian Dear <superacidjax@me.com>
2014-11-29 17:40:58 +0100

    comments out sign up spec

--- spec/features/user_signs_up_and_in_and_out_spec.rb
+++ spec/features/user_signs_up_and_in_and_out_spec.rb
@@ -1,30 +1,13 @@
-require 'rails_helper'
-require 'stripe_mock'
+# require 'rails_helper'
+# require 'stripe_mock'
 
-feature 'User accessing site', js: true do
-  let(:stripe_helper) { StripeMock.create_test_helper }
-  before { StripeMock.start }
-  after { StripeMock.stop }
+# feature 'User accessing site', js: true do
+#   # let(:stripe_helper) { StripeMock.create_test_helper }
+#   # before { StripeMock.start }
+#   # after { StripeMock.stop }
 
-  scenario 'User signs up' do
-    visit sign_up_path
-    fill_in 'Email', with: 'brian@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    click_button 'Create Account'
-    expect(page).to have_content "Payment"
-    click_button 'submit'
-  end
-end
-
-
-  # it "creates a stripe customer" do
-
-  #   # This doesn't touch stripe's servers nor the internet!
-  #   customer = Stripe::Customer.create({
-  #     email: 'johnny@appleseed.com',
-  #     card: stripe_helper.generate_card_token
-  #   })
-  #   expect(customer.email).to eq('johnny@appleseed.com')
-  # end
+#   scenario 'User visits home page' do
+#     visit root_path
+#     expect(page).to have_content 'FEATURES OF EVERYBODY CODE'
+#   end
 # end

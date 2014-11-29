f6a818907fbdf43dc29799ee6c2b9f19a2dd0fbc
Brian Dear <superacidjax@me.com>
2014-11-29 17:40:14 +0100

    updates Course fabricator

--- spec/fabricators/course_fabricator.rb
+++ spec/fabricators/course_fabricator.rb
@@ -1,4 +1,11 @@
 Fabricator(:course) do
   name { Faker::Company.catch_phrase }
-  price 300
+  price 30000
+  description { Faker::Lorem.paragraph }
+  video_url 108074329
+  slug  { Faker::Company.catch_phrase.parameterize }
+  programming_language 'Swift'
+  level 'Beginner'
+  device 'iOS'
+  headline { Faker::Lorem.sentence }
 end

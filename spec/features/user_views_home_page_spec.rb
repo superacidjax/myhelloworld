6a03500e49e67a5c05eaa272e8dc700fd7fbed55
Brian Dear <superacidjax@me.com>
2014-11-29 17:40:47 +0100

    adds spec for viewing home page

--- spec/features/user_views_home_page_spec.rb
+++ spec/features/user_views_home_page_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 feature 'User Views Homepage' do
 
-  scenario 'A user views the home page' do
-
+  scenario 'User visits home page' do
     visit root_path
-    expect(page).to have_content('Everybody Code!')
+    expect(page).to have_content 'Features of Everybody Code!'
   end
 end

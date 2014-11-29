f24b5691f6fe3a740611e276b5127adf99a90f58
Brian Dear <superacidjax@me.com>
2014-11-29 17:41:23 +0100

    comments out pending specs

--- spec/models/completion_spec.rb
+++ spec/models/completion_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Completion, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Completion, :type => :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end

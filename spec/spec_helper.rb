b98abe6bb0831e8ead37be81658df33ef75d53c3
Brian Dear <superacidjax@me.com>
2014-12-02 21:53:55 +0100

    removes clutter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,6 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end

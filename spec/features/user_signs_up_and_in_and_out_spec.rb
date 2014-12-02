0d1c483ab17998ad2cb3afb598b751a1b9202bfd
Brian Dear <superacidjax@me.com>
2014-12-02 21:42:11 +0100

    adds specs for signup failure

--- spec/features/user_signs_up_and_in_and_out_spec.rb
+++ spec/features/user_signs_up_and_in_and_out_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'User signs in, up and out' do
   let(:course) { Fabricate(:course) }
   let(:user) { Fabricate(:user) }
-  
+
   before do
     course.save
   end
@@ -19,6 +19,29 @@ feature 'User signs in, up and out' do
     expect(page).to have_content 'Welcome! You are now signed up.'
     expect(page).to have_content course.lessons.where(lesson_number: 1).last.name
   end
+
+  scenario 'User signs up with a bad email' do
+    visit courses_path
+    click_link course.id
+    click_link 'Get Started Now'
+    fill_in 'Display name', with: 'briandear'
+    fill_in 'Email', with: 'brian@examplecom'
+    fill_in 'Password', with: 'password'
+    click_button 'Try for Free'
+    expect(page).to have_content 'Email is invalid'
+  end
+
+  scenario 'User signs up with a bad email' do
+    visit courses_path
+    click_link course.id
+    click_link 'Get Started Now'
+    fill_in 'Display name', with: 'briandear'
+    fill_in 'Email', with: 'brian@example.com'
+    fill_in 'Password', with: ''
+    click_button 'Try for Free'
+    expect(page).to have_content "Password can't be blank"
+  end
+
 
   scenario 'Existing user signs in and signs out' do
     visit root_path

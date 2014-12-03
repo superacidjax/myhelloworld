bc1240ba6a10d1ed6d95a1f8e3e51757d0d66160
Brian Dear <superacidjax@me.com>
2014-12-03 23:08:18 +0100

    refactors charge method

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -43,24 +43,26 @@ class Charge < ActiveRecord::Base
 
   def charge_card
     begin
-      stripe_charge = Stripe::Charge.create(
-        amount: self.amount,
-        currency: "usd",
-        customer: self.customer_id,
-        description: self.course.name,
-        card: self.default_card
-      )
-      balance = Stripe::BalanceTransaction.retrieve(stripe_charge.balance_transaction)
-      self.update(
-        stripe_id:       stripe_charge.id,
-        card_expiration: Date.new(stripe_charge.card.exp_year, stripe_charge.card.exp_month, 1),
-        fee_amount:      (balance.fee)
-      )
+      stripe_charge = create_stripe_charge
+      update_charge(stripe_charge)
       self.finish!
     rescue Stripe::StripeError => e
       self.update_attributes(error: e.message)
       self.fail!
     end
+  end
+
+  def create_stripe_charge
+    Stripe::Charge.create(amount: self.amount, currency: "usd",
+      customer: self.customer_id, description: self.course.name,
+      card: self.default_card)
+  end
+
+  def update_charge(stripe_charge)
+    balance = Stripe::BalanceTransaction.retrieve(stripe_charge.balance_transaction)
+    self.update(stripe_id: stripe_charge.id,
+      card_expiration: Date.new(stripe_charge.card.exp_year, stripe_charge.card.exp_month, 1),
+      fee_amount: balance.fee)
   end
 
 end

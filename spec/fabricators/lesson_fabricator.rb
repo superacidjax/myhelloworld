fc6c2ee2e321dd00365545510550a7c7bc794610
Brian Dear <superacidjax@me.com>
2014-12-03 20:18:12 +0100

    changes to explicitly fake url

--- spec/fabricators/lesson_fabricator.rb
+++ spec/fabricators/lesson_fabricator.rb
@@ -4,7 +4,7 @@ Fabricator(:lesson) do
   description { Faker::Lorem.paragraph }
   video_url   12311231234
   notes { Faker::Lorem.paragraph }
-  thumbnail_url { Faker::Internet.url }
+  thumbnail_url 'http://example.com'
   duration { Random.new.rand(60) }
   lesson_number { sequence(:lesson_number) { |i| i } }
 end

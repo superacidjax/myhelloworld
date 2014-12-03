0f266eef1b715bc6e8b9ccd6c85c41e95f061021
Brian Dear <superacidjax@me.com>
2014-12-03 20:18:22 +0100

    refactors charges#create

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,7 +6,7 @@ class ChargesController < ApplicationController
   expose(:charges) { Charge.all.order("created_at DESC") }
 
   before_action :require_admin, except: [:new, :show, :create,
-    :apply_discount_code, :calculate_discount_price]
+                                         :apply_discount_code, :calculate_discount_price]
   before_action :require_login
 
   def new
@@ -28,61 +28,70 @@ class ChargesController < ApplicationController
   def create
     session[:discount_code_id] = nil
     self.course = Course.find(session[:course_id])
-
-
-    if customer = Stripe::Customer.create(
-      email: current_user.email,
-      card: params["stripeToken"]
-    )
-
-      if charge = course.charges.create(
-        amount: params[:final_sale_price],
-        user_id: current_user.id,
-        customer_id: customer.id,
-        course_id: course.id,
-        default_card: customer.default_card,
-        discount_code_id: params[:discount_code_id],
-        course_price: course.price,
-        access_expiration_date: Date.today + 1.year
-        )
-
-        StripeRunnerJob.perform_now charge.guid
-        flash[:success] = 'Thanks for your purchase!'
-        redirect_to user_dashboard_path
-      else
-        flash.now[:alert] = "We're sorry, it look like we had a connection issue!
-        Your card was NOT charged. Please try again!"
-        render :new
-      end
+    if charge = create_customer_and_charge(params, course)
+      StripeRunnerJob.perform_now charge.guid
+      flash[:success] = 'Thanks for your purchase!'
+      redirect_to user_dashboard_path
+    else
+      charge_error
     end
   end
 
-  def apply_discount_code
+  def create_customer_and_charge(params, course)
+    if customer = create_customer(params)
+      create_charge(params, customer, course)
+    end
   end
 
-  def calculate_discount_price
-    if params[:discount_code].present?
-      self.course = Course.find(session[:course_id])
-      if discount_code = DiscountCode.find_by(discount_code: params[:discount_code])
-        if discount_code.not_expired_or_cancelled?
-          session[:discount_code_id] = discount_code.id
-          flash[:success] = 'Discount applied!'
-          redirect_to new_charge_path(course_id: course.id)
-        else
-          flash[:error] = 'That discount code has expired!'
-          redirect_to new_charge_path(course_id: course.id)
-        end
+  def create_customer(params)
+    Stripe::Customer.create(email: current_user.email, card: params["stripeToken"])
+  end
+
+  def create_charge(params, customer, course)
+    course.charges.create(
+      amount: params[:final_sale_price],
+      user_id: current_user.id,
+      customer_id: customer.id,
+      course_id: course.id,
+      default_card: customer.default_card,
+      discount_code_id: params[:discount_code_id],
+      course_price: course.price,
+      access_expiration_date: Date.today + 1.year
+    )
+  end
+
+  def charge_error
+    flash.now[:alert] = "We're sorry, it look like we had a connection issue!
+          Your card was NOT charged. Please try again!"
+    render :new
+  end
+
+def apply_discount_code
+end
+
+def calculate_discount_price
+  if params[:discount_code].present?
+    self.course = Course.find(session[:course_id])
+    if discount_code = DiscountCode.find_by(discount_code: params[:discount_code])
+      if discount_code.not_expired_or_cancelled?
+        session[:discount_code_id] = discount_code.id
+        flash[:success] = 'Discount applied!'
+        redirect_to new_charge_path(course_id: course.id)
       else
-        flash[:error] = 'Sorry, but that code was not found.'
+        flash[:error] = 'That discount code has expired!'
         redirect_to new_charge_path(course_id: course.id)
       end
+    else
+      flash[:error] = 'Sorry, but that code was not found.'
+      redirect_to new_charge_path(course_id: course.id)
     end
   end
+end
 
-  private
+private
 
-  def charges_params
-    params.require(:charge).permit(:course_id, :user_id, :amount, :customer_id,
-      :access_expiration_date, :default_card)
-  end
+def charges_params
+  params.require(:charge).permit(:course_id, :user_id, :amount, :customer_id,
+                                 :access_expiration_date, :default_card)
+end
 end
